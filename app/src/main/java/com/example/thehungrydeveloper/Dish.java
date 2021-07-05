143e9d3b34909be8733d9f9f1a001c4514d68ebb
apoorv127 <prasada@uwm.edu>
2021-07-05 15:12:21 -0500

    Working with a 'Dish' Array

--- app/src/main/java/com/example/thehungrydeveloper/Dish.java
+++ app/src/main/java/com/example/thehungrydeveloper/Dish.java
@@ -1,5 +1,7 @@
 package com.example.thehungrydeveloper;
 
+import androidx.annotation.NonNull;
+
 public class Dish {
 
     String title;
@@ -10,5 +12,11 @@ public class Dish {
         this.title = title;
         this.description = description;
         this.price = price;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title;
     }
 }
