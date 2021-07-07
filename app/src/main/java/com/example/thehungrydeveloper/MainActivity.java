8eaf6d6f03ab21f4d19bdbd3b280e254bccfe303
apoorv127 <prasada@uwm.edu>
2021-07-07 07:39:58 -0500

    Add the Desserts Screen

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     CardView startersCard;
     CardView mainsCard;
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+        dessertsCard = findViewById(R.id.card_view_desserts);
 
 
         startersCard.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
                 startActivity(mainCoursesActivityIntent);
+            }
+        });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dessertsActivityIntent = new Intent(MainActivity.this, DessertsActivity.class);
+                startActivity(dessertsActivityIntent);
             }
         });
 
