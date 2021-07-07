9e353dd7c4b9ed78ab94df743c093243c8a1cdee
apoorv127 <prasada@uwm.edu>
2021-07-07 07:17:25 -0500

    Implicit Intents - Moving to another app

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
 
+
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
                 startActivity(mainCoursesActivityIntent);
+            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implicit Intents
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:thehungrydeveloper@gmail.com")); // only email apps should handle this
+                startActivity(launchEmailAppIntent);
+
             }
         });
     }
